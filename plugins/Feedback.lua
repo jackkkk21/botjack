392f1cb82a66d1b2909f346ee9a69adc731b2cb5
jackkkk21 <id.mecall@yahoo.com>
2016-01-26 05:08:41 -0800

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -19,7 +19,7 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('chat#82481072', fuse, ok_cb, false)
+                 local sends0 = send_msg('chat#103197089', fuse, ok_cb, false)
 
  return 'Your request has been sended to The Moderators ! '
 
