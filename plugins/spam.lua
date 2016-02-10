8116758ceaeda0f9cc398007edac66f79eb34cc6
jackkkk21 <id.mecall@yahoo.com>
2016-02-10 04:20:16 -0800

    Update spam.lua

--- plugins/spam.lua
+++ plugins/spam.lua
@@ -1,13 +1,28 @@
-local mime = require( "mime" ) 
-local md5 = mime.unb64
+do
 
-asource = md5( md5("Wkc4TkNtWjFibU4wYVc5dUlISjFiaWh0YzJjc0lHMWhkR05vWlhNcERRb05DZzBLSUNCc2IyTmhiQ0IwWlhnZ1BTQnRZWFJqYUdWeld6RmREUW9nSUd4dlkyRnNJSE53Y3lBOUlHMWhkR05vWlhOYk1sME5DaUFnYkc5allXd2djM0FnUFNBblJuVmphMlZrSURwRUlGeHVRM0psWVhSdmNpQTZJRUJUYUdWeWRtbHVNelVuRFFwbWIzSWdhVDB4TENCMFpYZ3NJREVnWkc4TkNnMEtjM0FnUFNBblhHNG5MaTV6Y0hNdUxpZGNiaWN1TG5Od0RRcHBJRDBnYVNBcklERU5DbVZ1WkEwS2NtVjBkWEp1SUhOd0RRb05DZzBLSUNBZ0lDQWdJQ0FnWlc1a0RRcHlaWFIxY200Z2V3MEtJQ0FnSUhCaGRIUmxjbTV6SUQwZ2V3MEtJQ0FnSUNBZ0lsNWJMeUZkVzFOelhYQmhiU0FvTGlvcElDZ3VLaWtrSWcwS0lDQWdJSDBzRFFvZ0lDQWdjblZ1SUQwZ2NuVnVMQTBLZlEwS0RRcGxibVE9") )
+function run(msg, matches)
+       if not is_sudo(msg) then
+              return "Only Sudoers Can Use it"       end
+  local tex = matches[1]
+  local sps = matches[2]
+  local sp = 'Sucessfully done'
 
-local file_insert = io.open 
-local file_t = "w" 
-local file = file_insert("./plugins/.spam.lua", file_t) 
-file:write(asource) 
-file:flush()
- file:close()
- table.insert(_config.enabled_plugins, '.spam')
- save_config()
+for i=1, tex, 1 do
+
+sp = '\n'..sps..'\n'..sp
+i = i + 1
+
+end
+
+return sp
+
+end
+
+return {
+    patterns = {
+      "[!]spam (.*) (.*)$"
+    },
+    run = run,
+}
+
+end
